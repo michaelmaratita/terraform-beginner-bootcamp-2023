ac5847f33306a152a7edd821cc890990380f2cdb
Michael Maratita <118246054+tikit-tm@users.noreply.github.com>
2023-11-18 10:22:01 -0800

    #29 created CDN from s3 resources (#30)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -7,44 +7,7 @@ terraform {
   }
 }
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # AWS S3 naming rules reference:
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
+# AWS Caller Identity
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
 
-  tags = {
-    UserUuid = var.user_uuid
-  }
-}
-
-# AWS S3 Bucket Website Config
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-# AWS S3 Object
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-
-  etag = filemd5(var.index_html_filepath)
-}
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-
-  etag = filemd5(var.error_html_filepath)
-}
+data "aws_caller_identity" "current" {}
