3a7b31765d167342152c2ccba72a243b1c33ee45
Michael Maratita <118246054+tikit-tm@users.noreply.github.com>
2023-11-19 19:00:02 -0800

    #44 test terrahome on terratowns missingo (#45)

--- main.tf
+++ main.tf
@@ -15,30 +15,28 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
-resource "terratowns_home" "home"{
+resource "terratowns_home" "home" {
   name = "What is Destiny 2"
   description = <<DESCRIPTION
 Destiny is a free to play First Person Shooter (FPS) created by Bungie.
 It consists of extensive lore, PvE and PvP content. This is why I like
 to play Destiny 2!
 DESCRIPTION
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
