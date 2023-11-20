a14901724c70aaaedddd6316d426c28f99546302
Michael Maratita <mikool671@gmail.com>
2023-11-20 02:59:12 +0000

    #44 test terrahome on terratowns missingo

--- varaiables.tf
+++ varaiables.tf
@@ -1,9 +1,15 @@
-variable "user_uuid" {
+variable "terratowns_access_token" {
   type = string
 }
-variable "bucket_name" {
+variable "terratowns_endpoint" {
   type = string
 }
+variable "teacherseat_user_uuid" {
+  type = string
+}
+# variable "bucket_name" {
+#   type = string
+# }
 variable "index_html_filepath" {
   type = string
 }
